f5cdddc85f7ce25f9749ca327d13b6cff9356674
lucasg <136121588+Lucasfront98@users.noreply.github.com>
2024-11-26 14:42:28 -0300

    exercício com tarefas usando gulp

---
+++ source/scripts/aritimetica.js
@@ -0,0 +1,8 @@
+const div = require('./sum');
+const sub = require('./sum');
+const somar = require('./sum')
+
+console.log('divisão: ', div(15, 3));
+console.log('subtração: ', sub(35, 10));
+console.log('somar: ', somar(15, 10));
+
---
+++ source/scripts/sum.js
@@ -0,0 +1,14 @@
+function somar(a, b) {
+    return a + b;
+}
+function sub(a, b) {
+    return a - b;
+}
+function div(a, b){
+    return a / b;
+}
+
+
+module.exports = somar;
+module.exports = sub;
+module.exports = div;
